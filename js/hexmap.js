4985b643e542dca412ee53a51089c532640b9955
Hannah Recht <hrecht@users.noreply.github.com>
2015-06-17 15:04:23 -0400

    create 3 crime map files

--- js/hexmap.js
+++ js/hexmap.js
@@ -1,9 +1,4 @@
-function crimemap() {
-    var max = 50,
-        scale,
-        classes = 7,
-        container = L.DomUtil.get('layers'),
-        scheme = colorbrewer["Reds"][classes];
+function crimemap(layers) {
 
     L.mapbox.accessToken = 'pk.eyJ1IjoidXJiYW5pbnN0aXR1dGUiLCJhIjoiTEJUbmNDcyJ9.mbuZTy4hI_PWXw3C3UFbDQ';
     var map = L.mapbox.map("map", 'urbaninstitute.iji12b2j', {
@@ -18,6 +13,88 @@ function crimemap() {
 
     var last_layer;
 
+    var control = document.getElementById('layers');
+
+    // Add a play button div
+    var play_button = control.appendChild(document.createElement('a'))
+    var pause = "&#9616;&#9616;";
+    var play = "&#9654;";
+    play_button.innerHTML = pause;
+    play_button.id = "play_button";
+    play_button.onclick = function () {
+        if (nextInterval) {
+            nextInterval = clearInterval(nextInterval);
+            play_button.innerHTML = play;
+        } else {
+            highlightLayer(i++);
+            nextInterval = animate();
+            play_button.innerHTML = pause;
+        }
+    }
+
+    layers.forEach(function (layer, n) {
+
+        layer.button = control.appendChild(document.createElement('a'));
+        layer.button.innerHTML = layers[n].name;
+        layer.button.onclick = function () {
+            highlightLayer(n);
+            i = n;
+            nextInterval = clearInterval(nextInterval);
+            play_button.innerHTML = play;
+        };
+    });
+
+    // we use a layer group to make it simple to remove an existing overlay
+    // and add a new one in the same line of code, as below, without juggling
+    // temporary variables.
+    var layerGroup = L.layerGroup().addTo(map);
+
+    // i is the number of the currently-selected layer
+    var i = 1;
+
+    // show the first overlay as soon as the map loads
+    highlightLayer(i++);
+
+    var nextInterval = animate();
+
+    function animate() {
+        // and then time the next() function to run every 1 seconds
+        return setInterval(function () {
+            highlightLayer(i);
+            if (++i >= layers.length) i = 0;
+        }, 1000 * 1);
+
+    }
+
+    function highlightLayer(i) {
+        layerGroup.clearLayers().addLayer(layers[i].layer);
+        var active = control.getElementsByClassName('active');
+        for (var j = 0; j < active.length; j++) active[j].className = '';
+        layers[i].button.className = 'active';
+    }
+
+}
+
+function assaultmap() {
+    var max = 50,
+        scale,
+        classes = 7,
+        container = L.DomUtil.get('layers'),
+        scheme = colorbrewer["Reds"][classes];
+
+    function hex_style(hexagons) {
+        // Maintain a density scale relative to initial zoom level.
+        scale = d3.scale.quantize()
+            .domain([0, max])
+            .range(d3.range(classes));
+
+        hexagons
+        // .attr("stroke", scheme[classes - 1])
+            .attr("fill", function (d) {
+            return scheme[scale(d.length)];
+        });
+    }
+
     function init(id) {
         var tile = new L.hexLayer(id, {
             applyStyle: hex_style
@@ -25,7 +102,7 @@ function crimemap() {
         return tile;
     }
 
-    var layers = [{
+    var assaultlayers = [{
             name: "2000",
             layer: init(assault2000)
      }, {
@@ -72,6 +149,15 @@ function crimemap() {
             layer: init(assault2014)
      }
 ];
+    crimemap(assaultlayers);
+}
+
+function robberymap() {
+    var max = 50,
+        scale,
+        classes = 7,
+        container = L.DomUtil.get('layers'),
+        scheme = colorbrewer["Reds"][classes];
 
     function hex_style(hexagons) {
         // Maintain a density scale relative to initial zoom level.
@@ -86,65 +172,134 @@ function crimemap() {
         });
     }
 
-    var control = document.getElementById('layers');
-
-    // Add a play button div
-    var play_button = control.appendChild(document.createElement('a'))
-    var pause = "&#9616;&#9616;";
-    var play = "&#9654;";
-    play_button.innerHTML = pause;
-    play_button.id = "play_button";
-    play_button.onclick = function () {
-        if (nextInterval) {
-            nextInterval = clearInterval(nextInterval);
-            play_button.innerHTML = play;
-        } else {
-            highlightLayer(i++);
-            nextInterval = animate();
-            play_button.innerHTML = pause;
-        }
+    function init(id) {
+        var tile = new L.hexLayer(id, {
+            applyStyle: hex_style
+        });
+        return tile;
     }
-
-    layers.forEach(function (layer, n) {
-
-        layer.button = control.appendChild(document.createElement('a'));
-        layer.button.innerHTML = layers[n].name;
-        layer.button.onclick = function () {
-            highlightLayer(n);
-            i = n;
-            nextInterval = clearInterval(nextInterval);
-            play_button.innerHTML = play;
-        };
-    });
-
-    // we use a layer group to make it simple to remove an existing overlay
-    // and add a new one in the same line of code, as below, without juggling
-    // temporary variables.
-    var layerGroup = L.layerGroup().addTo(map);
-
-    // i is the number of the currently-selected layer
-    var i = 0;
-
-    // show the first overlay as soon as the map loads
-    highlightLayer(i++);
-
-    var nextInterval = animate();
-
-    function animate() {
-        // and then time the next() function to run every 1 seconds
-        return setInterval(function () {
-            highlightLayer(i);
-            if (++i >= layers.length) i = 0;
-        }, 1000 * 1);
-
-    }
-
-    function highlightLayer(i) {
-        layerGroup.clearLayers().addLayer(layers[i].layer);
-        var active = control.getElementsByClassName('active');
-        for (var j = 0; j < active.length; j++) active[j].className = '';
-        layers[i].button.className = 'active';
-    }
-
+    var robberylayers = [{
+            name: "2000",
+            layer: init(robbery2000)
+     }, {
+            name: "2001",
+            layer: init(robbery2001)
+     }, {
+            name: "2002",
+            layer: init(robbery2002)
+     }, {
+            name: "2003",
+            layer: init(robbery2003)
+     }, {
+            name: "2004",
+            layer: init(robbery2004)
+     }, {
+            name: "2005",
+            layer: init(robbery2005)
+     }, {
+            name: "2006",
+            layer: init(robbery2006)
+     }, {
+            name: "2007",
+            layer: init(robbery2007)
+     }, {
+            name: "2008",
+            layer: init(robbery2008)
+     }, {
+            name: "2009",
+            layer: init(robbery2009)
+     }, {
+            name: "2010",
+            layer: init(robbery2010)
+     }, {
+            name: "2011",
+            layer: init(robbery2011)
+     }, {
+            name: "2012",
+            layer: init(robbery2012)
+     }, {
+            name: "2013",
+            layer: init(robbery2013)
+     }, {
+            name: "2014",
+            layer: init(robbery2014)
+     }
+];
+    crimemap(robberylayers);
 }
-crimemap();
+
+function homicidemap() {
+    var max = 8,
+        scale,
+        classes = 7,
+        container = L.DomUtil.get('layers'),
+        scheme = colorbrewer["Reds"][classes];
+
+    function hex_style(hexagons) {
+        // Maintain a density scale relative to initial zoom level.
+        scale = d3.scale.quantize()
+            .domain([0, max])
+            .range(d3.range(classes));
+
+        hexagons
+        // .attr("stroke", scheme[classes - 1])
+            .attr("fill", function (d) {
+            return scheme[scale(d.length)];
+        });
+    }
+
+    function init(id) {
+        var tile = new L.hexLayer(id, {
+            applyStyle: hex_style
+        });
+        return tile;
+    }
+    var homicidelayers = [{
+            name: "2000",
+            layer: init(homicide2000)
+     }, {
+            name: "2001",
+            layer: init(homicide2001)
+     }, {
+            name: "2002",
+            layer: init(homicide2002)
+     }, {
+            name: "2003",
+            layer: init(homicide2003)
+     }, {
+            name: "2004",
+            layer: init(homicide2004)
+     }, {
+            name: "2005",
+            layer: init(homicide2005)
+     }, {
+            name: "2006",
+            layer: init(homicide2006)
+     }, {
+            name: "2007",
+            layer: init(homicide2007)
+     }, {
+            name: "2008",
+            layer: init(homicide2008)
+     }, {
+            name: "2009",
+            layer: init(homicide2009)
+     }, {
+            name: "2010",
+            layer: init(homicide2010)
+     }, {
+            name: "2011",
+            layer: init(homicide2011)
+     }, {
+            name: "2012",
+            layer: init(homicide2012)
+     }, {
+            name: "2013",
+            layer: init(homicide2013)
+     }, {
+            name: "2014",
+            layer: init(homicide2014)
+     }
+];
+    crimemap(homicidelayers);
+}
