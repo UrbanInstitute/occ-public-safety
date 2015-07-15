377aa43104bff68b505706426b773fe1f520ed57
Hannah Recht <hrecht@users.noreply.github.com>
2015-07-15 12:33:08 -0400

    footnote, script update

--- scripts/csv2geojson.sh
+++ scripts/csv2geojson.sh
@@ -1,12 +1,6 @@
 for i in {2000..2014}
 do csv2geojson data/assault/assault$i.csv > data/assault/assault$i.geojson
-done
-
-for i in {2000..2014}
 do csv2geojson data/homicide/homicide$i.csv > data/homicide/homicide$i.geojson
-done
-
-for i in {2000..2014}
 do csv2geojson data/robbery/robbery$i.csv > data/robbery/robbery$i.geojson
 done
 
